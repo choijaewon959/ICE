ab0ff107bd5bd0863ebf5f268f31e47ac2c0d9d1
Jay Choi <choijaewon959@gmail.com>
2019-03-20 22:26:34 +0800

    mainpage design done

--- ICE_project/basic_pages/views.py
+++ ICE_project/basic_pages/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def mainPage_loggedOut_View(request):
+    return render(request, 'mainpage_loggedOut.html')
+    
 def loginPageView(request):
     return render(request, 'login.html')
+
